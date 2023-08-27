2027ccd2fccbcecc934838da22cd250bab8c7129
Aryan Sarode <117713349+CoderBoy18@users.noreply.github.com>
2023-08-27 12:02:48 +0530

    Add files via upload

---
+++ SpeakFunctionWindows.py
@@ -0,0 +1,11 @@
+# Import the win32com.client module to use the Windows SAPI interface
+import win32com.client as wincl
+
+# Create a speech synthesis object
+speak = wincl.Dispatch("SAPI.SpVoice")
+
+# List of strings to be spoken
+l = ["Rahul", "Nishant", "Harry"]
+
+# Use the Speak method to vocalize each string in the list
+speak.Speak(l)
